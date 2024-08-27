fcfc9c27376b94c255e5480edecbe5d40f6b293f
Kerby Eugene <1856753@cegepmontpetit.ca>
2024-08-27 09:45:06 -0400

    triangles

--- Devoirs/BonjourKotlin/src/main/kotlin/triangles.kt
+++ Devoirs/BonjourKotlin/src/main/kotlin/triangles.kt
@@ -1,4 +1,21 @@
-fun main(){
-    val hauteur=4
-    for(i in 1..hauteur)
+
+
+fun triangle(hauteur: Int): String {
+    val builder = StringBuilder()
+    for (i in 1..hauteur) {
+        for (j in 1..i) {
+            builder.append('*')
+        }
+        builder.append('\n')
+    }
+    return builder.toString()
+}
+
+fun main() {
+    val tailles = listOf(4, 8, 10, 100)
+
+    for (taille in tailles) {
+        println(triangle(taille))
+        println()
+    }
 }
